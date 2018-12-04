51e144936e3baebd3f6dcf22b813308b6f2ccc58
Dominik Witczak <domionato@gmail.com>
2018-12-04 18:53:24 +0100

    Dodano klasę KontoBankowe

--- src/Aktorzy/KontoBankowe.java
+++ src/Aktorzy/KontoBankowe.java
@@ -1,4 +1,24 @@
 package Aktorzy;
 
 public class KontoBankowe {
+    public Integer numerKonta;
+    public Integer stanKonta;
+
+    public KontoBankowe(Integer numerKonta, Integer stanKonta) {
+        this.numerKonta = numerKonta;
+        this.stanKonta = stanKonta;
+    }
+
+    public void przelejSrodkiNa(Integer kwota, KontoBankowe odbiorca) throws NiewystarczajacaLiczbaSrodkowException{
+        if(brakujeSrodkow(kwota));
+            throw new NiewystarczajacaLiczbaSrodkowException();
+    }
+
+    private boolean brakujeSrodkow(Integer kwota){
+        return kwota > stanKonta;
+    }
+
+    public class NiewystarczajacaLiczbaSrodkowException extends Throwable {
+
+    }
 }
